3c9f4dfbc6e171d475c31506139e444b72bc06d4
Kevin <kstamps2484@gmail.com>
2021-01-20 11:57:02 -0700

    add a container component with styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from '@material-ui/core/Container'
+import { makeStyles } from '@material-ui/core/styles'
+import './App.css'
 
-
+const useStyles = makeStyles((theme) => ({
+  container: {
+      background: 'linear-gradient(45deg, #113C70, #3D0757)',
+      height: 800
+  }
+}))
 
 function App() {
+  const classes = useStyles()
   return (
-    <div className="App">
-      
-    </div>
+    <Container className={classes.container} fixed>
+
+    </Container>
   );
 }
 
-export default App;
+export default App
