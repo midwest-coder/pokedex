025c359ec3da01ae83799e621a56940e7e8497ce
Kevin <kstamps2484@gmail.com>
2021-01-20 13:44:18 -0700

    add logic to fetch pokemon resources

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react'
 import Container from '@material-ui/core/Container'
 import { makeStyles } from '@material-ui/core/styles'
 import './App.css'
@@ -11,9 +12,28 @@ const useStyles = makeStyles((theme) => ({
 
 function App() {
   const classes = useStyles()
+  const [pokemonList, setPokemonList] = useState([])
+
+  useEffect(() => {
+     fetch('https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json').then((res) => {
+        return res.json().then((data) => data)
+        }).then((data) => {
+          const { pokemon } = data
+          console.log(pokemon)
+          setPokemonList(pokemon)
+        })
+  }, [])
+  
+
+  let content
+  pokemonList == null ?
+  content = '' : 
+  content = <h1>{pokemonList[0].name}</h1>
+  
+
   return (
     <Container className={classes.container} fixed>
-
+      {content}
     </Container>
   );
 }
